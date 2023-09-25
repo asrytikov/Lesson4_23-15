dd37da25794fbed2518d0ed22400adbf7edab7ac
asrytikov <asrytikov@gmail.com>
2023-09-25 09:11:33 +0400

    Lesson4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello. It's a calc");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input a first decimal: ");
+        int a = scanner.nextInt();
+        System.out.println("Input a second decimal: ");
+        int b = scanner.nextInt();
+
+        System.out.printf("Sum a=%d and b=%d: %d \n",a, b, a+b);
+
     }
 }
